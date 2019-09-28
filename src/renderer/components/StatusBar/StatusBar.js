972000d0caa937ff9f8d3828faad8d91c6c16a95
Reynold Mok <reyold@hotmail.com>
2019-09-28 23:01:43 +0800

    Add initial status bar

--- src/renderer/components/StatusBar/StatusBar.js
+++ src/renderer/components/StatusBar/StatusBar.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Icon } from 'antd';
+import './StatusBar.css';
 
 const StatusBar = () => {
   return (
     <div
-      style={{ width: '100%', height: 24, backgroundColor: '#1890ff' }}
-    ></div>
+      style={{
+        width: '100%',
+        height: 24,
+        color: 'white',
+        backgroundColor: '#1890ff',
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}
+    >
+      <div className="cea-ticker" style={{ margin: '0 10px' }}>
+        <text className="cea-ticker-text">Nothing Running</text>
+      </div>
+      <div style={{ margin: '0 10px' }}>
+        <Icon className="cea-status-bar-buttons" type="bell" theme="filled" />
+      </div>
+    </div>
   );
 };
 
