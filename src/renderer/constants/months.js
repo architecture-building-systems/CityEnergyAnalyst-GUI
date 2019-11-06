a826bab21b75eade9bb274aa21ed1cf10abf131c
Reynold Mok <reyold@hotmail.com>
2019-11-06 18:58:54 +0800

    Create month constants

---
+++ src/renderer/constants/months.js
@@ -0,0 +1,29 @@
+export default [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+export const months_short = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
