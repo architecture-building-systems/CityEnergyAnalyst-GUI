bf059f791b55bbd10958d56d08df96941ea0c755
Reynold Mok <reyold@hotmail.com>
2020-01-24 15:30:44 +0800

    Create function to fetch online version

--- src/renderer/utils/index.js
+++ src/renderer/utils/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export function checkNestedProp(obj, prop, ...rest) {
   if (typeof obj[prop] == 'undefined') return false;
@@ -48,4 +49,41 @@ export const AsyncError = ({
       )}
     </div>
   );
+};
+
+const versionRegex = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:[0-9A-Za-z-]+)?/;
+export async function fetchOnlineVersion() {
+  // Get latest tag from GitHub redirect URL
+  try {
+    const test = await axios.get(
+      'https://github.com/architecture-building-systems/CityEnergyAnalyst/releases/latest'
+    );
+    return test.request.res.responseUrl.match(versionRegex)[0];
+    // Could also use PyPi API (https://pypi.org/pypi/cityenergyanalyst/json)
+
+    // Offical Github API but is rate limiting
+    // const resp = await axios.get(
+    //   'https://api.github.com/repos/architecture-building-systems/CityEnergyAnalyst/releases/latest'
+    // );
+    // return resp.data.tag_name.match(versionRegex)[0];
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+export const versionCompare = (left, right) => {
+  // Returns 1 if left has higher version than right else -1
+  const verifyVersion = new RegExp(versionRegex);
+  if (!verifyVersion.test(left))
+    throw `${left} is not in a valid version format.`;
+  if (!verifyVersion.test(right))
+    throw `${right} is not in a valid version format.`;
+
+  const leftVersion = left.split('.');
+  const rightVersion = right.split('.');
+  for (let i = 0; i < leftVersion.length; i++) {
+    if (leftVersion[i] > rightVersion[i]) return 1;
+  }
+  return -1;
 };
