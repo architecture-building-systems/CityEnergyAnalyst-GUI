f61a9c23d20dade0c971bd9eab99530187be35fb
Reynold Mok <reyold@hotmail.com>
2020-02-09 23:45:10 +0800

    Combine redux actions to one init action

--- src/renderer/actions/databaseEditor.js
+++ src/renderer/actions/databaseEditor.js
@@ -1,73 +1,95 @@
 import { httpAction } from '../store/httpMiddleware';
 import axios from 'axios';
 
-const fetchDBData = async db => {
-  const resp = await axios.get(
-    `http://localhost:5050/api/inputs/databases/${db}`
-  );
-  return resp.data;
-};
-const fetchDBSchema = async db => {
-  const resp = await axios.get(
-    `http://localhost:5050/api/databases/schema/${db}`
-  );
-  return resp.data;
-};
-
-export const FETCH_DATABASE = 'FETCH_DATABASE';
-export const FETCH_DATABASE_SUCCESS = 'FETCH_DATABASE_SUCCESS';
-export const FETCH_DATABASE_FAILURE = 'FETCH_DATABASE_FAILURE';
-export const fetchDatabase = db => dispatch => {
-  const fetchAll = async () => {
-    try {
-      dispatch({ type: FETCH_DATABASE, payload: { db } });
-      // eslint-disable-next-line
-      const values = await Promise.all([fetchDBData(db), fetchDBSchema(db)]);
-      dispatch({
-        type: FETCH_DATABASE_SUCCESS,
-        payload: { data: values[0], schema: values[1], db }
-      });
-    } catch (err) {
-      dispatch({ type: FETCH_DATABASE_FAILURE, payload: { db, data: err } });
-    }
-  };
-
-  fetchAll();
+export const INIT_DATABASE_STATE = 'INIT_DATABASE_STATE';
+export const INIT_DATABASE_STATE_SUCCESS = 'INIT_DATABASE_STATE_SUCCESS';
+export const initDatabaseState = () => async dispatch => {
+  dispatch({ type: INIT_DATABASE_STATE });
+  // eslint-disable-next-line
+  return Promise.all([
+    dispatch(fetchDatabaseData('all')),
+    dispatch(fetchDatabaseSchema('all')),
+    dispatch(fetchDatabaseGlossary())
+  ]).then(() => dispatch({ type: INIT_DATABASE_STATE_SUCCESS }));
 };
 
-export const FETCH_ALL_DATABASES = 'FETCH_ALL_DATABASES';
-export const FETCH_ALL_DATABASES_SUCCESS = 'FETCH_ALL_DATABASES_SUCCESS';
-export const FETCH_ALL_DATABASES_FAILURE = 'FETCH_ALL_DATABASES_FAILURE';
-export const fetchAllDatabases = () => dispatch => {
-  const fetchAll = async () => {
-    try {
-      dispatch({ type: FETCH_ALL_DATABASES });
-      // eslint-disable-next-line
-      const values = await Promise.all([
-        fetchDBData('all'),
-        fetchDBSchema('all')
-      ]);
-      dispatch({
-        type: FETCH_ALL_DATABASES_SUCCESS,
-        payload: { data: values[0], schema: values[1] }
-      });
-    } catch (err) {
-      dispatch({ type: FETCH_ALL_DATABASES_FAILURE, payload: { data: err } });
-    }
-  };
+export const FETCH_DATABASE_DATA = 'FETCH_DATABASE_DATA';
+export const FETCH_DATABASE_DATA_SUCCESS = 'FETCH_DATABASE_DATA_SUCCESS';
+export const FETCH_DATABASE_DATA_FAILURE = 'FETCH_DATABASE_DATA_FAILURE';
+export const fetchDatabaseData = db => async dispatch => {
+  try {
+    dispatch({ type: FETCH_DATABASE_DATA });
+    const { data } = await axios.get(
+      `http://localhost:5050/api/inputs/databases/${db}`
+    );
+    return dispatch({
+      type: FETCH_DATABASE_DATA_SUCCESS,
+      payload: data
+    });
+  } catch (err) {
+    return dispatch({ type: FETCH_DATABASE_DATA_FAILURE, payload: err });
+  }
+};
 
-  fetchAll();
+export const FETCH_DATABASE_SCHEMA = 'FETCH_DATABASE_SCHEMA';
+export const FETCH_DATABASE_SCHEMA_SUCCESS = 'FETCH_DATABASE_SCHEMA_SUCCESS';
+export const FETCH_DATABASE_SCHEMA_FAILURE = 'FETCH_DATABASE_SCHEMA_FAILURE';
+export const fetchDatabaseSchema = db => async dispatch => {
+  try {
+    dispatch({ type: FETCH_DATABASE_SCHEMA });
+    const { data } = await axios.get(
+      `http://localhost:5050/api/databases/schema/${db}`
+    );
+    return dispatch({
+      type: FETCH_DATABASE_SCHEMA_SUCCESS,
+      payload: data
+    });
+  } catch (err) {
+    return dispatch({ type: FETCH_DATABASE_SCHEMA_FAILURE, payload: err });
+  }
 };
 
 export const FETCH_DATABASE_GLOSSARY = 'FETCH_DATABASE_GLOSSARY';
 export const FETCH_DATABASE_GLOSSARY_SUCCESS =
   'FETCH_DATABASE_GLOSSARY_SUCCESS';
-export const FETCH_DATABASE_GLOSSARY_FAILED = 'FETCH_DATABASE_GLOSSARY_FAILED';
-export const fetchDatabaseGlossary = () =>
-  httpAction({
-    type: FETCH_DATABASE_GLOSSARY,
-    url: 'http://localhost:5050/api/glossary/'
-  });
+export const FETCH_DATABASE_GLOSSARY_FAILURE =
+  'FETCH_DATABASE_GLOSSARY_FAILURE';
+export const fetchDatabaseGlossary = () => async dispatch => {
+  try {
+    dispatch({ type: FETCH_DATABASE_GLOSSARY });
+    const { data } = await axios.get(`http://localhost:5050/api/glossary`);
+    return dispatch({
+      type: FETCH_DATABASE_GLOSSARY_SUCCESS,
+      payload: data
+    });
+  } catch (err) {
+    return dispatch({ type: FETCH_DATABASE_GLOSSARY_FAILURE, payload: err });
+  }
+};
+
+// export const FETCH_ALL_DATABASES = 'FETCH_ALL_DATABASES';
+// export const FETCH_ALL_DATABASES_SUCCESS = 'FETCH_ALL_DATABASES_SUCCESS';
+// export const FETCH_ALL_DATABASES_FAILURE = 'FETCH_ALL_DATABASES_FAILURE';
+// export const fetchAllDatabases = () => dispatch => {
+//   const fetchAll = async () => {
+//     try {
+//       dispatch({ type: FETCH_ALL_DATABASES });
+//       // eslint-disable-next-line
+//       const values = await Promise.all([
+//         fetchDBData('all'),
+//         fetchDBSchema('all')
+//       ]);
+//       dispatch({
+//         type: FETCH_ALL_DATABASES_SUCCESS,
+//         payload: { data: values[0], schema: values[1] }
+//       });
+//     } catch (err) {
+//       dispatch({ type: FETCH_ALL_DATABASES_FAILURE, payload: { data: err } });
+//     }
+//   };
+
+//   fetchAll();
+// };
 
 export const UPDATE_DATABASE_VALIDATION = 'UPDATE_DATABASE_VALIDATION';
 export const updateDatabaseValidation = validation => dispatch => {
@@ -79,3 +101,15 @@ export const updateDatabaseState = () => ({ type: UPDATE_DATABASE_STATE });
 
 export const RESET_DATABASE_STATE = 'RESET_DATABASE_STATE';
 export const resetDatabaseState = () => ({ type: RESET_DATABASE_STATE });
+
+export const SET_DATABASE_CATAGORY_TAB = 'SET_DATABASE_CATAGORY_TAB';
+export const setDatabaseCatagoryTab = catagory => ({
+  type: SET_DATABASE_CATAGORY_TAB,
+  payload: catagory
+});
+
+export const SET_DATABASE_NAME_TAB = 'SET_DATABASE_NAME_TAB';
+export const setDatabaseNameTab = name => ({
+  type: SET_DATABASE_NAME_TAB,
+  payload: name
+});
--- src/renderer/reducers/databaseEditor.js
+++ src/renderer/reducers/databaseEditor.js
@@ -1,13 +1,17 @@
 import {
   UPDATE_DATABASE_STATE,
   RESET_DATABASE_STATE,
+  FETCH_DATABASE_DATA_SUCCESS,
   UPDATE_DATABASE_VALIDATION,
   FETCH_DATABASE_GLOSSARY_SUCCESS,
-  FETCH_ALL_DATABASES_SUCCESS,
-  FETCH_ALL_DATABASES_FAILURE,
-  FETCH_ALL_DATABASES,
   SET_DATABASE_CATAGORY_TAB,
-  SET_DATABASE_NAME_TAB
+  SET_DATABASE_NAME_TAB,
+  FETCH_DATABASE_SCHEMA_SUCCESS,
+  INIT_DATABASE_STATE,
+  INIT_DATABASE_STATE_SUCCESS,
+  FETCH_DATABASE_DATA_FAILURE,
+  FETCH_DATABASE_SCHEMA_FAILURE,
+  FETCH_DATABASE_GLOSSARY_FAILURE
 } from '../actions/databaseEditor';
 import { combineReducers } from 'redux';
 import { checkNestedProp, createNestedProp, deleteNestedProp } from '../utils';
@@ -58,39 +62,71 @@ const databaseGlossary = (state = [], { type, payload }) => {
   }
 };
 
-const databaseData = (state = { data: {}, schema: {} }, { type, payload }) => {
+const databaseData = (state = {}, { type, payload }) => {
   switch (type) {
     case UPDATE_DATABASE_STATE:
+      console.log(payload);
       return state;
-    case FETCH_ALL_DATABASES:
-      return { ...state, status: 'fetching' };
-    case FETCH_ALL_DATABASES_SUCCESS:
-      return {
-        data: payload.data,
-        schema: payload.schema
-      };
-    case FETCH_ALL_DATABASES_FAILURE:
-      return {
-        ...state,
-        status: 'failed',
-        error: payload.data
-      };
+    case FETCH_DATABASE_DATA_SUCCESS:
+      return payload;
     case RESET_DATABASE_STATE:
-      return { data: {}, schema: {} };
+      return {};
     default:
       return state;
   }
 };
 
+const databaseSchema = (state = {}, { type, payload }) => {
+  switch (type) {
+    case FETCH_DATABASE_SCHEMA_SUCCESS:
+      return payload;
+    case RESET_DATABASE_STATE:
+      return {};
+    default:
+      return state;
+  }
+};
+
+const databaseStatus = (state = { status: null }, { type, payload }) => {
+  switch (type) {
+    case INIT_DATABASE_STATE:
+      return { status: 'fetching' };
+    case INIT_DATABASE_STATE_SUCCESS:
+      return { status: 'success' };
+    case FETCH_DATABASE_DATA_FAILURE:
+    case FETCH_DATABASE_SCHEMA_FAILURE:
+    case FETCH_DATABASE_GLOSSARY_FAILURE:
+      return { status: 'failed', error: payload };
+    case RESET_DATABASE_STATE:
+      return { status: null };
+    default:
+      return state;
+  }
+};
+
+const databaseTabs = (
+  state = { category: null, name: null },
+  { type, payload }
+) => {
+  switch (type) {
+    case SET_DATABASE_CATAGORY_TAB:
+      return { ...state, category: payload };
+    case SET_DATABASE_NAME_TAB:
+      return { ...state, name: payload };
+    case RESET_DATABASE_STATE:
+      return { category: null, name: null };
     default:
       return state;
   }
 };
 
 const databaseEditor = combineReducers({
+  status: databaseStatus,
   validation: databaseValidation,
-  databases: databaseData,
+  data: databaseData,
+  schema: databaseSchema,
   glossary: databaseGlossary,
+  tabs: databaseTabs
 });
 
 export default databaseEditor;
