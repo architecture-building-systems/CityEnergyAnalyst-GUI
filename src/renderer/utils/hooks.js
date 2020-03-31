01e9ff12685ff74f723e786b294a223e553e084f
Reynold Mok <reyold@hotmail.com>
2020-03-31 17:35:43 +0800

    Add useEventListener hook

--- src/renderer/utils/hooks.js
+++ src/renderer/utils/hooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export const useAsyncData = (
@@ -27,4 +27,27 @@ export const useAsyncData = (
   }, [...dependecies]);
 
   return [data, isLoading, error];
+};
+
+export const useEventListener = (eventName, handler, element = window) => {
+  const savedHandler = useRef();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    // Make sure element supports addEventListener
+    const isSupported = element && element.addEventListener;
+
+    if (!isSupported) return;
+
+    const eventListener = event => savedHandler.current(event);
+    element.addEventListener(eventName, eventListener);
+
+    // Remove event listener on cleanup
+    return () => {
+      element.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element]);
 };
