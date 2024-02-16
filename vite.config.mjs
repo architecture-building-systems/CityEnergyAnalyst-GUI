e1ed35494db53393007f555215dc9bc080ca75a1
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-02-16 15:06:28 +0100

    Remove unused plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,33 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-    },
-  },
-  plugins: [
-    react(),
-    vitePluginImp({
-      libList: [
-        {
-          libName: 'antd',
-          style: (name) => `antd/es/${name}/style`,
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
 });
