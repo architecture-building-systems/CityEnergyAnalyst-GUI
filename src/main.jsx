a14f92164c7972d7aca91b93e9e4a8d8400fe9ff
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-03-04 17:16:11 +0100

    Remove axios logs

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './containers/App';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
-import axios from 'axios';
 
 const store = configureStore();
-
-axios.interceptors.request.use((request) => {
-  console.debug('Starting Request', JSON.stringify(request, null, 2));
-  return request;
-});
-
-axios.interceptors.response.use((response) => {
-  console.log('Response:', JSON.stringify(response, null, 2));
-  return response;
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
