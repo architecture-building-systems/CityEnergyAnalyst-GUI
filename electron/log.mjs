138b57d9c3db8ff912fa5c64bf64de49a0b93a19
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-03-27 16:29:53 +0100

    Add electron-log

---
+++ electron/log.mjs
@@ -0,0 +1,10 @@
+import log from 'electron-log';
+
+export const initLog = () => {
+  // Optional, initialize the logger for any renderer process
+  log.initialize();
+
+  console.log = log.log;
+  console.error = log.error;
+  console.debug = log.debug;
+};
