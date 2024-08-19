2fccf7b0d105f3cbf589f45b1b3d15d630f37cec
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-08-19 15:55:41 +0200

    Add typology validation

--- src/components/Project/CreateScenarioForms/TypologyForm.jsx
+++ src/components/Project/CreateScenarioForms/TypologyForm.jsx
@@ -1,6 +1,29 @@
-import { Button, Form } from 'antd';
-import { OpenDialogInput } from '../../Tools/Parameter';
+import { Button, Divider, Form, Select } from 'antd';
+import { OpenDialogButton } from '../../Tools/Parameter';
 import { useEffect } from 'react';
+import axios from 'axios';
+import { FileSearchOutlined } from '@ant-design/icons';
+import { GENERATE_TYPOLOGY_CEA } from './constants';
+
+const validateTypology = async (value) => {
+  if (value === GENERATE_TYPOLOGY_CEA) return Promise.resolve();
+
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_CEA_URL}/api/geometry/typology/validate`,
+      {
+        type: 'path',
+        path: value,
+      },
+    );
+    console.log(response);
+    return Promise.resolve();
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.detail || 'Unable to validate typology.';
+    return Promise.reject(`${errorMessage}`);
+  }
+};
 
 const TypologyForm = ({
   initialValues,
@@ -37,13 +60,37 @@ const TypologyForm = ({
             <div>You can leave it empty and modify it later.</div>
           </div>
         }
+        rules={[{ validator: (_, value) => validateTypology(value) }]}
       >
-        <OpenDialogInput
-          form={form}
-          name="typology"
-          type="file"
-          filters={[{ name: 'DBF files', extensions: ['dbf'] }]}
-          placeholder="Leave empty to auto-generate default"
+        <Select
+          placeholder="Choose an option from the dropdown"
+          dropdownRender={(menu) => (
+            <div>
+              {menu}
+              <Divider style={{ margin: '4px 0' }} />
+              <OpenDialogButton
+                form={form}
+                name="typology"
+                type="file"
+                filters={[{ name: 'DBF files', extensions: ['dbf'] }]}
+                placeholder="Or enter path to typology file here"
+              >
+                <FileSearchOutlined />
+                Browse for typology file
+              </OpenDialogButton>
+            </div>
+          )}
+          options={[
+            {
+              label: 'Defaults',
+              options: [
+                {
+                  label: 'Auto-generate using default values',
+                  value: GENERATE_TYPOLOGY_CEA,
+                },
+              ],
+            },
+          ]}
         />
       </Form.Item>
 
--- src/components/Project/CreateScenarioForms/constants.js
+++ src/components/Project/CreateScenarioForms/constants.js
@@ -1,4 +1,5 @@
 export const GENERATE_ZONE_CEA = 'generate-zone-cea';
 export const GENERATE_SURROUNDINGS_CEA = 'generate-surroundings-cea';
+export const GENERATE_TYPOLOGY_CEA = 'generate-typology-cea';
 export const EMTPY_GEOMETRY = 'none';
 export const EXAMPLE_CITIES = ['Singapore', 'Zürich'];
