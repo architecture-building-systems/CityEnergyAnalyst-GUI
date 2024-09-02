cf3139fc47adf0d4d75b7dbb9b7b88335a8f5fe0
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-09-02 15:05:43 +0200

    Restructure landing layout

---
+++ src/components/Landing/Buttons/NewProjectModalButton.jsx
@@ -0,0 +1,37 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { push } from 'connected-react-router';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import NewProjectModal from '../../Project/NewProjectModal';
+
+import routes from '../../../constants/routes.json';
+
+const NewProjectModalButton = () => {
+  const [visible, setVisible] = useState(false);
+  const dispatch = useDispatch();
+
+  const goToProjectPage = () => {
+    dispatch(push(routes.PROJECT_OVERVIEW));
+  };
+
+  return (
+    <>
+      <Button
+        type="primary"
+        style={{ width: '100%' }}
+        onClick={() => setVisible(true)}
+      >
+        <PlusOutlined />
+        New Project
+      </Button>
+      <NewProjectModal
+        visible={visible}
+        setVisible={setVisible}
+        onSuccess={goToProjectPage}
+      />
+    </>
+  );
+};
+
+export default NewProjectModalButton;
