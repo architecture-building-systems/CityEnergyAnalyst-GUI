d812f2dc3abce96a8010083f9e137a81ee833581
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-09-02 15:04:18 +0200

    Restructure app layout

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,8 +1,5 @@
 import { Suspense, lazy, useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import SideNav from '../components/HomePage/SideNav';
-import Header from '../components/HomePage/Header';
-import { ToolRoute } from '../components/Tools/Tool';
 
 import routes from '../constants/routes.json';
 import { useDispatch, useSelector } from 'react-redux';
@@ -23,9 +20,7 @@ const ContextProvider = ({ children }) => {
   );
 };
 
-const Project = lazy(() => import('../components/Project/Project'));
 const CreateScenario = lazy(() => import('./CreateScenario'));
-const InputEditor = lazy(() => import('../components/InputEditor/InputEditor'));
 const Dashboard = lazy(() => import('../components/Dashboard/Dashboard'));
 const DatabaseEditor = lazy(
   () => import('../components/DatabaseEditor/DatabaseEditor'),
@@ -34,43 +29,30 @@ const Landing = lazy(() => import('../components/Landing/Landing'));
 
 const HomePageContent = () => {
   return (
-    <div id="homepage-content">
-      <ErrorBoundary>
-        <Switch>
-          <Route path={routes.PROJECT_OVERVIEW}>
-            <Suspense>
-              <Project />
-            </Suspense>
-          </Route>
-          <Route path={routes.CREATE_SCENARIO}>
-            <Suspense>
-              <CreateScenario />
-            </Suspense>
-          </Route>
-          <Route path={routes.INPUT_EDITOR}>
-            <Suspense>
-              <InputEditor />
-            </Suspense>
-          </Route>
-          <Route path={`${routes.TOOLS}/:script`} component={ToolRoute} />
-          <Route path={routes.DASHBOARD}>
-            <Suspense>
-              <Dashboard />
-            </Suspense>
-          </Route>
-          <Route path={routes.DATABASE_EDITOR}>
-            <Suspense>
-              <DatabaseEditor />
-            </Suspense>
-          </Route>
-          <Route exact path={routes.HOME}>
-            <Suspense>
-              <Landing />
-            </Suspense>
-          </Route>
-        </Switch>
-      </ErrorBoundary>
-    </div>
+    <ErrorBoundary>
+      <Switch>
+        <Route path={routes.CREATE_SCENARIO}>
+          <Suspense>
+            <CreateScenario />
+          </Suspense>
+        </Route>
+        <Route path={routes.DASHBOARD}>
+          <Suspense>
+            <Dashboard />
+          </Suspense>
+        </Route>
+        <Route path={routes.DATABASE_EDITOR}>
+          <Suspense>
+            <DatabaseEditor />
+          </Suspense>
+        </Route>
+        <Route exact path={routes.HOME}>
+          <Suspense>
+            <Landing />
+          </Suspense>
+        </Route>
+      </Switch>
+    </ErrorBoundary>
   );
 };
 
@@ -93,18 +75,8 @@ const HomePage = () => {
   return (
     <ContextProvider>
       <div id="homepage-container">
-        <div id="homepage-top-container">
-          <div id="homepage-sidebar-container">
-            <SideNav />
-          </div>
-          <div id="homepage-right-container">
-            <div id="homepage-header-container">
-              <Header />
-            </div>
-            <div id="homepage-content-container">
-              <HomePageContent />
-            </div>
-          </div>
+        <div id="homepage-content-container">
+          <HomePageContent />
         </div>
         <div id="homepage-status-bar-container">
           <StatusBar />
