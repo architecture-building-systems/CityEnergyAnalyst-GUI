be91d37e9ffa5fa4d2d6f8e6ec6995e0d71e6d37
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-09-02 10:52:15 +0200

    Use select when project options exists

--- src/components/Project/OpenProjectModal.jsx
+++ src/components/Project/OpenProjectModal.jsx
@@ -1,37 +1,34 @@
-import { useRef, useState, useEffect } from 'react';
-import { Form } from '@ant-design/compatible';
-import { Modal } from 'antd';
-import { FormItemWrapper, OpenDialogInput } from '../Tools/Parameter';
+import { useState, useEffect, useMemo } from 'react';
+import { Form, Modal, Select } from 'antd';
+import { OpenDialogInput } from '../Tools/Parameter';
 import { useFetchConfigProjectInfo } from '../Project/Project';
-import { checkExist } from '../../utils/file';
+import { checkExist, dirname, joinPath } from '../../utils/file';
 import { useFetchProject } from '../../utils/hooks';
 
 const OpenProjectModal = ({ visible, setVisible, onSuccess = () => {} }) => {
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const formRef = useRef();
-  const {
-    info: { project },
-    fetchInfo,
-  } = useFetchConfigProjectInfo();
+  const [form] = Form.useForm();
+  const { info, fetchInfo } = useFetchConfigProjectInfo();
+  const { project } = info;
   const fetchProject = useFetchProject();
 
   useEffect(() => {
     if (visible) fetchInfo();
   }, [visible]);
 
-  const handleOk = () => {
-    formRef.current.validateFields(async (err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-        setConfirmLoading(true);
-        const { project } = values;
-        fetchProject(project).then(() => {
-          setConfirmLoading(false);
-          setVisible(false);
-          onSuccess();
-        });
-      }
-    });
+  const handleOk = async () => {
+    try {
+      const values = await form.validateFields();
+      setConfirmLoading(true);
+      const { project } = values;
+      fetchProject(project).then(() => {
+        setConfirmLoading(false);
+        setVisible(false);
+        onSuccess();
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const handleCancel = () => {
@@ -49,35 +46,56 @@ const OpenProjectModal = ({ visible, setVisible, onSuccess = () => {} }) => {
       confirmLoading={confirmLoading}
       destroyOnClose
     >
-      <OpenProjectForm ref={formRef} initialValue={project} />
+      {project && (
+        <OpenProjectForm form={form} initialValue={project} project={info} />
+      )}
     </Modal>
   );
 };
 
-const OpenProjectForm = Form.create()(({ form, initialValue }) => {
+const OpenProjectForm = ({ form, initialValue, project }) => {
+  const projectOptions = useMemo(() => {
+    // FIXME: This is a workaround to get the project root
+    const projectRoot = project?.project ? dirname(project?.project) : '';
+    const projectList = project?.projects || [];
+
+    return projectList.map((projectName) => ({
+      label: projectName,
+      value: joinPath(projectRoot, projectName),
+    }));
+  }, [project]);
+
   return (
-    <Form>
-      <FormItemWrapper
-        form={form}
+    <Form
+      form={form}
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 15, offset: 1 }}
+    >
+      <Form.Item
+        label="Project"
         name="project"
         initialValue={initialValue}
-        help="Path of Project"
+        extra="Path of Project"
         rules={[
           {
-            validator: async (rule, value, callback) => {
+            validator: async (_, value) => {
               const pathExists = await checkExist('', 'directory', value);
               if (!pathExists) {
-                callback('Path entered is invalid');
-              } else {
-                callback();
+                return Promise.reject('Path entered is invalid');
               }
+              return Promise.resolve();
             },
           },
         ]}
-        inputComponent={<OpenDialogInput form={form} type="directory" />}
-      />
+      >
+        {projectOptions ? (
+          <Select placeholder="project_root" options={projectOptions} />
+        ) : (
+          <OpenDialogInput form={form} type="directory" />
+        )}
+      </Form.Item>
     </Form>
   );
-});
+};
 
 export default OpenProjectModal;
