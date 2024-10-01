07b621232c3118f496ae80802c01d6ea81c9473b
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-10-01 04:44:18 +0200

    Check internet using cityenergyanalyst.com instead of google.com
    Using google.com could cause some issues with some users in places where is google is blocked, where the GUI would not launch.

--- electron/utils.mjs
+++ electron/utils.mjs
@@ -2,7 +2,7 @@ import dns from 'dns';
 
 export async function checkInternet() {
   try {
-    await dns.promises.lookup('google.com');
+    await dns.promises.lookup('www.cityenergyanalyst.com');
     return true; // Internet is connected
   } catch (err) {
     if (err.code === 'ENOTFOUND') {
