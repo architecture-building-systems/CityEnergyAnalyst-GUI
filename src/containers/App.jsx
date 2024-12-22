1d7117a3ac1e1b3731fe57e4e22ed3fd19f8f368
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-12-22 23:37:02 +0100

    Convert text divs into svg

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -15,13 +15,48 @@ const HomePage = lazy(() =>
 
 const Splash = lazy(() => import('../components/Splash/Splash'));
 
+const CeaBadge = () => (
+  <div
+    style={{
+      display: 'flex',
+      width: 'fit-content',
+      background: 'rgb(69,77,84)',
+      borderRadius: 8,
+      padding: '3px 8px',
+      color: '#fff',
+      fontSize: 24,
+      fontWeight: 'bold',
+    }}
+  >
+    CEA 4
+  </div>
+);
+
 const Loading = () => {
+  const ceaText = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 120"
+      fontSize={30}
+      fill="currentColor"
+    >
+      <text x="0" y="30">
+        City
+      </text>
+      <text x="0" y="70">
+        Energy
+      </text>
+      <text x="0" y="110">
+        Analyst
+      </text>
+    </svg>
+  );
+
   return (
     <div
       className="cea-loading"
       style={{
         height: '100vh',
-        width: '100vw',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
@@ -29,46 +64,46 @@ const Loading = () => {
         gap: 24,
       }}
     >
+      <div style={{ width: '10%', minWidth: 100 }}>
+        <CeaLogoAnimate />
+      </div>
+
       <div
         style={{
-          minWidth: 200,
-          width: '18%',
           display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          flexDirection: 'column',
+          gap: 8,
 
-          gap: 24,
+          width: '10%',
+          minWidth: 100,
+
+          fontFamily: 'Arial',
+          color: 'rgb(69,77,84)',
         }}
       >
-        <CeaLogoAnimate />
-        <div
-          style={{
-            fontSize: 28,
-            color: 'rgb(69,77,84)',
-            fontFamily: 'Arial',
-          }}
-        >
-          <div>City</div>
-          <div>Energy</div>
-          <div>Analyst</div>
-          <div
-            style={{
-              display: 'flex',
-              width: 'fit-content',
-              background: 'rgb(69,77,84)',
-              borderRadius: 8,
-              padding: '3px 8px',
-              color: '#fff',
-              marginTop: 8,
-              fontSize: 24,
-              fontWeight: 'bold',
-            }}
+        <div style={{ width: '50%' }}>{ceaText}</div>
+        <div style={{ width: '60%' }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 85 33"
+            fontSize={24}
           >
-            CEA 4
-          </div>
+            <g>
+              <rect
+                x="0"
+                y="0"
+                width="85"
+                height="33"
+                rx="8"
+                fill="rgb(69,77,84)"
+              />
+              <text x="8" y="25" fill="white" fontWeight="bold">
+                CEA 4
+              </text>
+            </g>
+          </svg>
         </div>
       </div>
-      {/* <img src={ceaLogo} style={{ width: 350 }} alt="CEA Logo" /> */}
     </div>
   );
 };
