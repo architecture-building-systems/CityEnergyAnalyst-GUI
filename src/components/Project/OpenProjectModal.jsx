419d1427ceb10584f1700c54c474f42973725874
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-01-14 16:15:54 +0100

    Use project choices when opening from browser

--- src/components/Project/OpenProjectModal.jsx
+++ src/components/Project/OpenProjectModal.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { Form, Modal } from 'antd';
+import { useEffect, useState } from 'react';
+import { Form, Modal, Select } from 'antd';
 import { OpenDialogInput } from '../Tools/Parameter';
 import { checkExist } from '../../utils/file';
 import { useProjectStore } from './store';
+import { isElectron } from '../../utils/electron';
+import axios from 'axios';
 
 const OpenProjectModal = ({ visible, setVisible, onSuccess }) => {
   const project = useProjectStore((state) => state.project);
@@ -46,6 +48,68 @@ const OpenProjectModal = ({ visible, setVisible, onSuccess }) => {
   );
 };
 
+const PathDialog = ({ initialValue }) => {
+  return (
+    <Form.Item
+      label="Project"
+      name="project"
+      initialValue={initialValue}
+      extra="Path of Project"
+      rules={[
+        {
+          validator: async (_, value) => {
+            if (value.length == 0)
+              return Promise.reject('Project cannot be empty');
+            await checkExist(value, 'directory');
+            return Promise.resolve();
+          },
+        },
+      ]}
+    >
+      <OpenDialogInput type="directory" />
+    </Form.Item>
+  );
+};
+
+const useFetchProjectChoices = () => {
+  const [choices, setChoices] = useState();
+
+  const fetchInfo = async () => {
+    try {
+      const resp = await axios.get(
+        `${import.meta.env.VITE_CEA_URL}/api/project/choices`,
+      );
+      const _choices = resp.data?.projects;
+      setChoices(_choices);
+    } catch (err) {
+      console.error(err?.response?.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return choices;
+};
+
+const ProjectChoice = ({ initialValue }) => {
+  const choices = useFetchProjectChoices();
+
+  const options = choices
+    ? choices.map((choice) => ({
+        label: choice,
+        value: choice,
+      }))
+    : [];
+
+  return (
+    <Form.Item label="Project" name="project">
+      <Select placeholder="Select a project" options={options} />
+    </Form.Item>
+  );
+};
+
 const OpenProjectForm = ({ form, onFinish, initialValue }) => {
   return (
     <Form
@@ -54,24 +118,11 @@ const OpenProjectForm = ({ form, onFinish, initialValue }) => {
       labelCol={{ span: 6 }}
       wrapperCol={{ span: 15, offset: 1 }}
     >
-      <Form.Item
-        label="Project"
-        name="project"
-        initialValue={initialValue}
-        extra="Path of Project"
-        rules={[
-          {
-            validator: async (_, value) => {
-              if (value.length == 0)
-                return Promise.reject('Project cannot be empty');
-              await checkExist(value, 'directory');
-              return Promise.resolve();
-            },
-          },
-        ]}
-      >
-        <OpenDialogInput type="directory" />
-      </Form.Item>
+      {isElectron() ? (
+        <PathDialog initialValue={initialValue} />
+      ) : (
+        <ProjectChoice initialValue={initialValue} />
+      )}
     </Form>
   );
 };
