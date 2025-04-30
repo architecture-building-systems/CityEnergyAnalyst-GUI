cda7cd565ae63777381bb4994d4e26a8627e20db
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-04-30 10:47:38 +0800

    Round up for max range

--- src/components/Map/Layers/Selectors/Threhold.jsx
+++ src/components/Map/Layers/Selectors/Threhold.jsx
@@ -2,6 +2,18 @@ import { Slider } from 'antd';
 import { formatNumber } from '../../utils';
 import { useMemo, useState } from 'react';
 
+const ceilToSignificantDigits = (num, digits) => {
+  if (num === 0) return 0;
+  if (!Number.isFinite(num)) return num; // Handle Infinity or NaN
+  if (digits <= 0) return Math.ceil(num); // Handle non-positive digits
+
+  const magnitude = Math.pow(
+    10,
+    digits - Math.floor(Math.log10(Math.abs(num))) - 1,
+  );
+  return Math.ceil(num * magnitude) / magnitude;
+};
+
 const ThresholdSelector = ({ label, value, defaultValue, range, onChange }) => {
   const [threshold, setThreshold] = useState(value ?? defaultValue);
 
@@ -25,6 +37,12 @@ const ThresholdSelector = ({ label, value, defaultValue, range, onChange }) => {
     onChange?.(value);
   };
 
+  const roundedMin = useMemo(() => Number(range[0].toPrecision(3)), [range]);
+  const roundedMax = useMemo(
+    () => ceilToSignificantDigits(range[1], 3),
+    [range],
+  );
+
   return (
     <div>
       <div style={{ display: 'flex', gap: 8 }}>
@@ -36,8 +54,8 @@ const ThresholdSelector = ({ label, value, defaultValue, range, onChange }) => {
       <div style={{ paddingLeft: 12, paddingRight: 12 }}>
         <Slider
           value={threshold}
-          min={Number(range[0].toPrecision(3))}
-          max={Number(range[1].toPrecision(3))}
+          min={roundedMin}
+          max={roundedMax}
           defaultValue={defaultValue}
           range={{ draggableTrack: true }}
           onChange={handleChange}
