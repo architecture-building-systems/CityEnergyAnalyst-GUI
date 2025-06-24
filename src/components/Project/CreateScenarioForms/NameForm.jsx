302061f16d0341d9cc02879d0813bc31cf65943b
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-06-24 09:46:35 +0200

    Check for invalid characters in scenario name

--- src/components/Project/CreateScenarioForms/NameForm.jsx
+++ src/components/Project/CreateScenarioForms/NameForm.jsx
@@ -27,6 +27,23 @@ const NameForm = ({ initialValues, onFinish, formButtons }) => {
     if (scenarioNames.includes(value)) {
       return Promise.reject('Scenario name already exists.');
     }
+    // Path traversal and separator checks
+    if (value && (/\.\./.test(value) || /\//.test(value) || /\\/.test(value))) {
+      return Promise.reject(
+        'Scenario name cannot contain characters like "..", "/", or "\\".',
+      );
+    }
+    // Windows invalid characters
+    if (value && /[<>:"'|?*]/.test(value)) {
+      return Promise.reject(
+        'Scenario name cannot contain the characters < > : " \' | ? *',
+      );
+    }
+    // Check length
+    if (value && value.length > 255) {
+      return Promise.reject('Scenario name cannot exceed 255 characters.');
+    }
+
     return Promise.resolve();
   };
 
