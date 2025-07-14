957c9e00525b936d7307a22752110f0153283934
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-07-14 03:01:28 +0200

    Remove unused useAsyncData hook from hooks.jsx
    The useAsyncData hook and its related imports have been removed from hooks.jsx as they are no longer used.

--- src/utils/hooks.jsx
+++ src/utils/hooks.jsx
@@ -1,34 +1,5 @@
-import { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
+import { useEffect, useRef } from 'react';
 import useNavigationStore from 'stores/navigationStore';
-
-export const useAsyncData = (
-  url = '',
-  initialState = null,
-  dependecies = [],
-) => {
-  const [data, setData] = useState(initialState);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const resp = await axios.get(url);
-        console.log(resp.data);
-        setData(resp.data);
-      } catch (err) {
-        console.error(err.response.data);
-        setError(err.response.data);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetch();
-  }, [...dependecies]);
-
-  return [data, isLoading, error];
-};
 
 export const useEventListener = (eventName, handler, element = window) => {
   const savedHandler = useRef();
