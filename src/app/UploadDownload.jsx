e96c0b5dde908d0edfca603b9cf56830db8f31e7
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-07-14 02:56:56 +0200

    Move UploadForm and DownloadForm to feature folder
    Relocated UploadForm and DownloadForm components to src/features/upload-download/components and updated imports in UploadDownload.jsx for better project structure and organization.

--- src/app/UploadDownload.jsx
+++ src/app/UploadDownload.jsx
@@ -1,6 +1,6 @@
 import { Alert, Col, Row } from 'antd';
-import UploadForm from 'components/UploadForm';
-import DownloadForm from 'components/DownloadForm';
+import UploadForm from 'features/upload-download/components/UploadForm';
+import DownloadForm from 'features/upload-download/components/DownloadForm';
 
 const style = {
   height: '100%',
