dfcdb105c04a9eb163e78471650cd425abde49b1
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-07-14 02:29:15 +0200

    Refactor file structure and update imports
    Renamed App.jsx to index.jsx and app.global.css to index.css for consistency. Updated import paths in main.jsx to reflect these changes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from 'app/App';
-import 'app.global.css';
+import App from 'app';
+import 'index.css';
 
 import '@ant-design/v5-patch-for-react-19';
 
