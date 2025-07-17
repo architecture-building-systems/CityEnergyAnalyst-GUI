b4bb60a0406ef77b774fecfd42bf1f8132fc58ee
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-07-17 16:20:42 +0200

    Fix style of ShowHideCardsButton

--- src/components/ShowHideCardsButton.jsx
+++ src/components/ShowHideCardsButton.jsx
@@ -6,20 +6,22 @@ import { useHoverGrow } from 'features/project/hooks/hover-grow';
 export const ShowHideCardsButton = ({ hideAll, onToggle, style }) => {
   const { styles, onMouseEnter, onMouseLeave } = useHoverGrow();
   return (
-    <Tooltip title={!hideAll ? 'Show Overlays' : 'Hide Overlays'}>
-      <animated.div
-        className="cea-overlay-card"
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        style={styles}
-      >
-        <ShowHideCardsIcon
-          className="cea-card-toolbar-icon"
-          style={{ margin: 0, background: '#000', ...style }}
+    <animated.div
+      className={`cea-overlay-card cea-card-toolbar-icon-container ${!hideAll ? '' : 'inverted'}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      style={styles}
+    >
+      <Tooltip title={!hideAll ? 'Show Overlays' : 'Hide Overlays'}>
+        <button
+          className="cea-card-toolbar-icon no-hover-color"
           onClick={() => onToggle?.((prev) => !prev)}
-        />
-      </animated.div>
-    </Tooltip>
+          type="button"
+        >
+          <ShowHideCardsIcon style={style} />
+        </button>
+      </Tooltip>
+    </animated.div>
   );
 };
 
