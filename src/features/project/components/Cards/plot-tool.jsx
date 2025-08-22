f148f2c3f745c15f9e8a7b1fbfacdde5b1accc93
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-08-22 19:07:54 +0200

    Add icon to button

--- src/features/project/components/Cards/plot-tool.jsx
+++ src/features/project/components/Cards/plot-tool.jsx
@@ -3,7 +3,7 @@ import { Button, ConfigProvider, Form } from 'antd';
 import { PLOTS_PRIMARY_COLOR } from 'constants/theme';
 import { useEffect } from 'react';
 import { useMapStore } from 'features/map/stores/mapStore';
-import { VIEW_PLOT_RESULTS } from 'features/plots/constants';
+import { iconMap, VIEW_PLOT_RESULTS } from 'features/plots/constants';
 
 const PlotChoices = ({ onSelected }) => {
   const choices = Object.keys(VIEW_PLOT_RESULTS).filter(
@@ -13,14 +13,18 @@ const PlotChoices = ({ onSelected }) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
       <h2>Select a Plot Tool</h2>
-      {choices.map((choice) => (
-        <Button
-          key={choice}
-          onClick={() => onSelected(VIEW_PLOT_RESULTS[choice])}
-        >
-          {choice}
-        </Button>
-      ))}
+      {choices.map((choice) => {
+        const Icon = iconMap?.[choice] || null; // Fallback to null if no icon is found
+        return (
+          <Button
+            key={choice}
+            icon={<Icon />}
+            onClick={() => onSelected(VIEW_PLOT_RESULTS[choice])}
+          >
+            {choice}
+          </Button>
+        );
+      })}
     </div>
   );
 };
